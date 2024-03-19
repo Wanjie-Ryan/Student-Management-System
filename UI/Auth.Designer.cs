9d710fbd6ce30e118d22935cef63e44ac51e947e
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-19 21:15:09 +0300

    Add project files.

---
+++ UI/Auth.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Student_Management_System.UI
+{
+    partial class Auth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Auth
+            // 
+            AutoScaleDimensions = new SizeF(12F, 22F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Silver;
+            ClientSize = new Size(1000, 550);
+            Font = new Font("Modern No. 20", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Auth";
+            Text = "Auth";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
