541d122b5497007b2781633537d2e7a4aac7bd46
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-27 12:00:32 +0300

    teacher dashboard design

--- UI/Teacher.Designer.cs
+++ UI/Teacher.Designer.cs
@@ -28,22 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStripTeachers = new MenuStrip();
+            assignmentsToolStripMenuItem = new ToolStripMenuItem();
+            studentsToolStripMenuItem = new ToolStripMenuItem();
+            menuStripTeachers.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStripTeachers
+            // 
+            menuStripTeachers.Font = new Font("Modern No. 20", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            menuStripTeachers.ImageScalingSize = new Size(20, 20);
+            menuStripTeachers.Items.AddRange(new ToolStripItem[] { assignmentsToolStripMenuItem, studentsToolStripMenuItem });
+            menuStripTeachers.Location = new Point(0, 0);
+            menuStripTeachers.Name = "menuStripTeachers";
+            menuStripTeachers.Size = new Size(1200, 30);
+            menuStripTeachers.TabIndex = 0;
+            menuStripTeachers.Text = "menuStrip1";
+            // 
+            // assignmentsToolStripMenuItem
+            // 
+            assignmentsToolStripMenuItem.Name = "assignmentsToolStripMenuItem";
+            assignmentsToolStripMenuItem.Size = new Size(106, 24);
+            assignmentsToolStripMenuItem.Text = "Assignments";
+            // 
+            // studentsToolStripMenuItem
+            // 
+            studentsToolStripMenuItem.Name = "studentsToolStripMenuItem";
+            studentsToolStripMenuItem.Size = new Size(101, 26);
+            studentsToolStripMenuItem.Text = "Students";
             // 
             // Teacher
             // 
             AutoScaleDimensions = new SizeF(12F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1200, 600);
+            Controls.Add(menuStripTeachers);
             Font = new Font("Modern No. 20", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.None;
+            MainMenuStrip = menuStripTeachers;
             Margin = new Padding(4, 3, 4, 3);
             Name = "Teacher";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Teacher";
+            menuStripTeachers.ResumeLayout(false);
+            menuStripTeachers.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStripTeachers;
+        private ToolStripMenuItem assignmentsToolStripMenuItem;
+        private ToolStripMenuItem studentsToolStripMenuItem;
     }
 }
