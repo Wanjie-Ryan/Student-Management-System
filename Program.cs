0d02ec651f0652ae5d836cc88a5ecc3cc7351d01
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-28 09:57:49 +0300

    created the logics for the registration component

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Student_Management_System.UI;
+using System;
+using System.Windows.Forms;
 
 namespace Student_Management_System
 {
@@ -7,6 +9,19 @@ namespace Student_Management_System
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// 
+         // Define connection string variables
+        private static string server = "localhost";
+        private static string database = "schoolmanagement";
+        private static string username = "root";
+        private static string password = "1234567890";
+
+        public static string GetConnectionString()
+        {
+            // Construct and return the connection string
+            return $"server={server};database={database};uid={username};password={password};";
+        }
+
         [STAThread]
         static void Main()
         {
