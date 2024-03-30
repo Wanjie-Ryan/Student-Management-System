ee091def55a304108fd5c59b3087c976142d999c
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-30 16:35:13 +0300

    Database manipulation for Assignments

--- Data Layer/AssignmentDAL.cs
+++ Data Layer/AssignmentDAL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Student_Management_System.Logics;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,54 @@ namespace Student_Management_System.Data_Layer
 {
     internal class AssignmentDAL
     {
+
+        public DataTable Select()
+        {
+            DataTable dt = new DataTable();
+
+            using (MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            {
+                try
+                {
+                    string sql = "SELECT * FROM assignment";
+                    MySqlCommand cmd = new MySqlCommand(sql,conn);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    conn.Open();
+                    adapter.Fill(dt);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return dt;
+        }
+       public bool Insert(AssignmentBLL b)
+        {
+            bool isSuccess = false;
+
+            using(MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            {
+                try
+                {
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return isSuccess;
+        }
     }
 }
