03aa88a0c10a2ef76aaae29bddef461fa2d2d692
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-31 14:51:21 +0300

    Student module DB manipulation

--- Data Layer/CheckStudentsDAL.cs
+++ Data Layer/CheckStudentsDAL.cs
@@ -1,10 +1,10 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Student_Management_System.Data_Layer
 {
@@ -14,14 +14,54 @@ namespace Student_Management_System.Data_Layer
         {
             DataTable dt = new DataTable();
 
-            using(MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            using (MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
             {
                 try
                 {
-                    string sql = "SELECT id, username, role, first_name, last_name, reg_no FROM users";
+                    string sql =
+                        "SELECT id, username, role, first_name, last_name, reg_no FROM users";
                     MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    conn.Open();
+                    adapter.Fill(dt);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return dt;
+        }
+
+        public DataTable Search(string keyword)
+        {
+            DataTable dt = new DataTable();
+
+            using (MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            {
+                try
+                {
+                    string sql =
+                        "SELECT id, username, role, first_name, last_name, reg_no FROM users WHERE username LIKE '%"
+                        + keyword
+                        + "%' OR first_name LIKE '%"
+                        + keyword
+                        + "%' OR last_name LIKE '%"
+                        + keyword
+                        + "%' OR reg_no LIKE '%"
+                        + keyword
+                        + "%' ";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    conn.Open();
+                    adapter.Fill(dt);
+                }
+                catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message);
                 }
